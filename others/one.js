f4b39564febe6e4c23ab550e1cf4da7f02646819
gangadharpr <https://github.com/gangadharpr/Hi.git>
2022-12-14 13:30:58 +0530

    HTMl

---
+++ others/one.js
@@ -0,0 +1,13 @@
+let devices = [
+    {
+      name: 'iPhone',
+      brand: 'Apple',
+      color:'blue'
+    },
+    {
+      name: 'Galaxy',
+      brand: 'Samsung',
+      color:'red'
+    }
+  ]
+  console.table(devices, ['brand','name','color']);
